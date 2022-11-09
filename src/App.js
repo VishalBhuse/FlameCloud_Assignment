c20f2757701a4195139173e2f23456a999ff4531
VishalBhuse <vbhuse1997@gmail.com>
2022-11-09 08:19:47 +0530

    insert loader

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Sop } from "./Pages/Sop";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeout);
+      setLoading(false);
+    };
+  }, []);
+
+  if (loading) {
+    return (
+      <img
+        className="loader"
+        alt="loader"
+        src="https://cdn.dribbble.com/users/2346349/screenshots/9246147/media/06971345603f8422d664fa0ea362b3a5.gif"
+      />
+    );
+  }
+
   return (
     <>
       <Sop />
